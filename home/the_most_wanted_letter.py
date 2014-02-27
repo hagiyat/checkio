5eb1de51e5234747e04ae705001d9e362a560aeb
hagiyat <hagiyat@gmail.com>
2014-02-27 21:50:57 +0900

    テスト失敗の通知とかあるんだ。。すごいですね

--- home/the_most_wanted_letter.py
+++ home/the_most_wanted_letter.py
@@ -1,11 +1,12 @@
 import re
 def checkio(data):
+    data = list(re.sub(r'[^a-z]', '', data.lower()))
     counter = {}
-    for letter in list(re.sub(r'[^a-z]', '', data.lower())):
+    for letter in data:
         counter[letter] = counter.get(letter, 0) + 1
     result = [key for key,value in counter.items() if value > 1 and value >= max(counter.values())]
 
-    return sorted(result)[0] if result else list(data)[-1]
+    return sorted(result)[0] if result else sorted(data)[0]
 
 if __name__ == '__main__':
     assert(checkio("Hello World!") == "l")
@@ -14,3 +15,4 @@ if __name__ == '__main__':
     assert(checkio("Oops!") == "o")
     assert(checkio("AAaooo!!!!") == "a")
     assert(checkio("Lorem ipsum dolor sit amet") == "m")
+    assert checkio("abe") == "a"
